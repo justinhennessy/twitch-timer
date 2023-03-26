b31be994a5b67f6d519014fe38737d446138998d
Justin Hennessy <justinhennessy@Administrators-MacBook-Pro.local>
2023-03-27 08:17:09 +1000

    Rewrote timer so it actually works with external curl calls

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
+import time
 
 app = Flask(__name__)
-remaining_time = 300  # 5 minutes in seconds
+initial_seconds = 5 * 60
+max_seconds = 10 * 60
+remaining_seconds = initial_seconds
+last_updated = time.time()
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/timer')
+@app.route('/timer', methods=['GET'])
 def timer():
-    return render_template('timer.html', remaining_time=remaining_time)
+    global remaining_seconds, last_updated
 
-@app.route('/add_time')
+    current_time = time.time()
+    elapsed_time = current_time - last_updated
+    remaining_seconds -= elapsed_time
+    last_updated = current_time
+
+    if remaining_seconds < 0:
+        remaining_seconds = 0
+
+    return jsonify({"value": remaining_seconds})
+
+@app.route('/add-time', methods=['POST'])
 def add_time():
-    global remaining_time
-    remaining_time += 30  # Add 30 seconds to the remaining time
-    if remaining_time > 600:  # Limit the timer to 10 minutes
-        remaining_time = 600
-    return render_template('timer.html', remaining_time=remaining_time)
+    global remaining_seconds, last_updated
 
-@app.route('/nightbot', methods=['GET', 'POST'])
-def nightbot():
-    global remaining_time
-    if request.method == 'GET':
-        command = request.args.get('command')
-    elif request.method == 'POST':
-        command = request.form.get('command')
-    else:
-        return 'Invalid request method'
+    remaining_seconds = min(remaining_seconds + 30, max_seconds)
+    last_updated = time.time()
 
-    if command == '!encore':
-        remaining_time += 30  # Add 30 seconds to the remaining time
-        if remaining_time > 600:  # Limit the timer to 10 minutes
-            remaining_time = 600
-        return 'OK'
-    else:
-        return 'Invalid command'
+    return jsonify({"value": remaining_seconds})
+
+@app.route('/sub-time', methods=['POST'])
+def sub_time():
+    global remaining_seconds, last_updated
+
+    remaining_seconds = max(remaining_seconds - 30, 0)
+    last_updated = time.time()
+
+    return jsonify({"value": remaining_seconds})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,44 @@
+const counterElement = document.getElementById('counter');
+const increaseCounterButton = document.getElementById('increaseCounter');
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.round(seconds % 60);
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+function updateCounter(value) {
+  counterElement.textContent = formatTime(value);
+}
+
+async function fetchTimerValue() {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/timer');
+    if (response.ok) {
+      const data = await response.json();
+      updateCounter(data.value);
+    } else {
+      console.error('Error fetching timer value:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error fetching timer value:', error);
+  }
+}
+
+async function increaseTimerValue() {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/add-time', { method: 'POST' });
+    if (response.ok) {
+      const data = await response.json();
+      updateCounter(data.value);
+    } else {
+      console.error('Error increasing timer value:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error increasing timer value:', error);
+  }
+}
+
+setInterval(fetchTimerValue, 1000);
+increaseCounterButton.addEventListener('click', increaseTimerValue);
+
