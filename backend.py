8fb357ebc4fe543bc7bfedff873321dc7a76322e
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2023-09-10 23:20:52 +1000

    Added in threaded process to do the count down

--- backend.py
+++ backend.py
@@ -1,13 +1,29 @@
 from flask import Flask, jsonify, request
 import os
 from flask_cors import CORS
-from timer_manager import timer_manager
 import logging
+import threading
+import time
+from timer_manager import TimerManager
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 CORS(app, origins="*")
+
+def countdown_timer():
+    while True:
+        time.sleep(2)  # Sleep for 2 seconds
+        timer_manager.reduce_time(2)  # Reduce the timer by 2 seconds
+
+# Create a TimerManager instance and reset the timer to 5 minutes
+timer_manager = TimerManager()
+timer_manager.reset_time()
+
+# Start the countdown timer in a separate thread
+countdown_thread = threading.Thread(target=countdown_timer)
+countdown_thread.daemon = True
+countdown_thread.start()
 
 @app.route("/api/timer", methods=['GET'])
 def get_timer():
@@ -32,7 +48,4 @@ def reset_time():
     return jsonify({"message": "Timer reset to 5 minutes", "timer": timer_manager.get_remaining_time()})
 
 if __name__ == "__main__":
-    timer_manager = TimerManager()
-    timer_manager.reset_time()
     app.run(host='0.0.0.0', debug=True)
-
