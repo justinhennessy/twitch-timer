c9095f34fc26ac15d8edc4b2e244d9ae5fc15999
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2023-09-10 22:29:15 +1000

    Updated timer_manager to try and fix the intermittent timer issue

--- timer_manager.py
+++ timer_manager.py
@@ -1,35 +1,39 @@
-import time
+from datetime import datetime, timedelta
+import threading
 
 class TimerManager:
     def __init__(self):
-        self.start_time = time.time()
-        self.default_time = 5 * 60  # 5 minutes in seconds
+        self.start_time = datetime.now()
+        self.default_time = timedelta(minutes=5)
         self.total_set_time = self.default_time
-        self.maximum_time = 10 * 60  # 10 minutes in seconds
+        self.maximum_time = timedelta(minutes=10)
+        self.lock = threading.Lock()
 
     def get_remaining_time(self):
-        elapsed_time = time.time() - self.start_time
-        remaining_time = self.total_set_time - elapsed_time
+        with self.lock:
+            elapsed_time = datetime.now() - self.start_time
+            remaining_time = self.total_set_time - elapsed_time
 
-        if remaining_time <= 0:
-            return "Times up!"
-        
-        minutes = int(remaining_time // 60)
-        seconds = int(remaining_time % 60)
-        return f"{minutes}:{seconds:02}"
+            if remaining_time <= timedelta(seconds=0):
+                return "Times up!"
+
+            minutes, seconds = divmod(remaining_time.seconds, 60)
+            return f"{minutes}:{seconds:02}"
 
     def add_time(self):
-        self.total_set_time += 30
-        if self.total_set_time >= self.maximum_time:
-            self.total_set_time = self.maximum_time
+        with self.lock:
+            self.total_set_time += timedelta(seconds=30)
+            if self.total_set_time >= self.maximum_time:
+                self.total_set_time = self.maximum_time
 
     def reduce_time(self):
-        elapsed_time = time.time() - self.start_time
-        if self.total_set_time - elapsed_time > 0:
-            self.total_set_time -= 30
+        with self.lock:
+            elapsed_time = datetime.now() - self.start_time
+            if self.total_set_time - elapsed_time > timedelta(seconds=0):
+                self.total_set_time -= timedelta(seconds=30)
 
     def reset_time(self):
-        self.start_time = time.time()
-        self.total_set_time = self.default_time
+        with self.lock:
+            self.start_time = datetime.now()
+            self.total_set_time = self.default_time
 
-timer_manager = TimerManager()
