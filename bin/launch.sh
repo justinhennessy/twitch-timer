e3b3a6e3fbb4621f3306f2ced455318074ad2723
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2023-10-10 20:47:53 +1000

    Updated port for backend api service to 5001

--- bin/launch.sh
+++ bin/launch.sh
@@ -3,7 +3,7 @@
 # List of commands to run in separate panes with custom titles
 commands=(
     "echo 'Counter service is starting ...'; python count_down.py"
-    "echo 'ngrok is starting ...'; ngrok http 5000"
+    "echo 'ngrok is starting ...'; ngrok http 5001"
     "echo 'Backend API starting ...'; python backend.py"
 )
 
@@ -18,7 +18,7 @@ titles=(
 for i in "${!commands[@]}"; do
     cmd="${commands[i]}"
     title="${titles[i]}"
-    
+
     osascript -e "tell application \"iTerm\"
         activate
         set newWindow to (create window with default profile)
