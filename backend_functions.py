c8963c93081b201b30d095b508f4a6352ae757df
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2024-06-21 11:23:10 +1000

    Changed log level to INFO

--- backend_functions.py
+++ backend_functions.py
@@ -9,7 +9,7 @@ from timer_manager import TimerManager
 # Load environment variables from .env file
 load_dotenv()
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 bucket_name = os.getenv('BUCKET_NAME')
