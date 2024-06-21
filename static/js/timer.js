797ec2105374be1e1e464a91dff2b1f44ade9d4e
Justin Hennessy <292017+justinhennessy@users.noreply.github.com>
2024-06-21 11:57:19 +1000

    Extracted javascript from timer.html and put it in its own .js file

---
+++ static/js/timer.js
@@ -0,0 +1,67 @@
+function formatTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+}
+
+function displayTimerValue() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const uuid = urlParams.get('uuid');
+
+    if (!uuid) {
+        document.getElementById('timerDisplay').innerText = 'No timer specified!';
+        return;
+    }
+
+    fetch(`/api/timer/${uuid}`)
+        .then(response => response.json())
+        .then(data => {
+            const timerDisplay = document.getElementById('timerDisplay');
+            const body = document.body;
+            const infiniteImage = document.getElementById('infiniteImage');
+
+            if (data.timer === -999) {
+                timerDisplay.innerText = ""; // Clear the text
+                infiniteImage.style.display = 'block';
+
+                // Remove background animation class when timer is -999
+                body.classList.remove('pulsing-yellow', 'pulsing-red');
+                return;  // Exit the function early
+            }
+
+            infiniteImage.style.display = 'none'; // Hide the image if not in infinite mode
+
+            if (data.timer === -1) {
+                timerDisplay.innerText = "Let's Go!";
+                body.classList.remove('pulsing-yellow', 'pulsing-red');
+                return;
+            }
+
+            if (data.timer <= 30 && data.timer > 15) {
+                const formattedTime = formatTime(data.timer);
+                timerDisplay.innerText = formattedTime;
+                body.classList.remove('pulsing-red');
+                body.classList.add('pulsing-yellow');
+            } else if (data.timer <= 15 && data.timer > 0) {
+                const formattedTime = formatTime(data.timer);
+                timerDisplay.innerText = formattedTime;
+                body.classList.remove('pulsing-yellow');
+                body.classList.add('pulsing-red');
+            } else if (data.timer === 0) {
+                timerDisplay.innerText = 'Times up!';
+                body.classList.remove('pulsing-yellow', 'pulsing-red');
+                body.style.backgroundColor = 'white';
+            } else {
+                const formattedTime = formatTime(data.timer);
+                timerDisplay.innerText = formattedTime;
+                body.classList.remove('pulsing-yellow', 'pulsing-red');
+            }
+        })
+        .catch(error => {
+            console.error('Error in displayTimerValue:', error);
+            document.getElementById('timerDisplay').innerText = 'Error!';
+        });
+}
+
+displayTimerValue();
+setInterval(displayTimerValue, 1000);
